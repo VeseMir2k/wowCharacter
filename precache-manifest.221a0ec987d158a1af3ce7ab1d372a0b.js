a3610a03ccae9c22cf8594d5d237817fa801a089
Adam Stepien || VeseMir2k <vesemir2k@gmail.com>
2020-09-01 16:56:41 +0200

    Updates

---
+++ precache-manifest.221a0ec987d158a1af3ce7ab1d372a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b071708688e19577b976bc8a6bd16759",
+    "url": "/wowCharacter/index.html"
+  },
+  {
+    "revision": "931297e0fea8fe94dff4",
+    "url": "/wowCharacter/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6101acd311c1b595d24a",
+    "url": "/wowCharacter/static/css/main.ebd04a46.chunk.css"
+  },
+  {
+    "revision": "931297e0fea8fe94dff4",
+    "url": "/wowCharacter/static/js/2.1098e866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wowCharacter/static/js/2.1098e866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6101acd311c1b595d24a",
+    "url": "/wowCharacter/static/js/main.76db6691.chunk.js"
+  },
+  {
+    "revision": "cdcf786fbd87a684f6b5",
+    "url": "/wowCharacter/static/js/runtime-main.3bf593be.js"
+  }
+]);
